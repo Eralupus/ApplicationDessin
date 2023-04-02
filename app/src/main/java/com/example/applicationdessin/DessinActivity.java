edca044869dbcd8039d6818c14b6749e71928284
DINH Tran Thai Duc <ducthai2001177@gmail.com>
2023-04-02 19:16:00 +0200

    Delete classes Metier- Utiliser Bitmap

--- app/src/main/java/com/example/applicationdessin/DessinActivity.java
+++ app/src/main/java/com/example/applicationdessin/DessinActivity.java
@@ -1,10 +1,12 @@
 package com.example.applicationdessin;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Rect;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,19 +15,14 @@ import android.view.MotionEvent;
 import android.widget.FrameLayout;
 
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.applicationdessin.metier.Dessin;
-import com.example.applicationdessin.metier.Metier;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 
 public class DessinActivity extends AppCompatActivity {
 
     ViewDraw whatIdraw;
-
+    private Bitmap mBitmap;
+    private Bitmap mTempBitmap;
+    private Canvas mCanvas,mTempCanvas;
     private Paint paint;
 
 
@@ -33,9 +30,18 @@ public class DessinActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dessin);
         this.paint = new Paint();
+
         FrameLayout container = findViewById(R.id.custom_view_container);
         whatIdraw = new ViewDraw(this);
         container.addView(whatIdraw);
+
+        // Set up the Bitmap object to store the drawing
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int height = getResources().getDisplayMetrics().heightPixels;
+        mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+        mTempBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        mTempCanvas = new Canvas(mTempBitmap);
     }
 
 
@@ -58,128 +64,157 @@ public class DessinActivity extends AppCompatActivity {
 
     class ViewDraw extends View implements View.OnTouchListener, View.OnClickListener
     {
-        private Metier  metier = new Metier();
-        private final int TOOL_RECTANGLE = 1;
-        private final int TOOL_ROND      = 2;
-        private final int TOOL_TRACE     = 3;
-        private final int TOOL_ERASER    = 4;
-        private int currentTool = TOOL_RECTANGLE;
-        private int currentColor = Color.BLACK;
         private Path path = new Path();
-        private ArrayList<Path> lstPath = new ArrayList<Path>();
+        private final int TOOL_RECTANGLE = 1;
+        private final int TOOL_ROUND     = 2;
+        private final int TOOL_GOMME     = 3;
+        private final int TOOL_TRACE     = 4;
+        private final int currentTool = TOOL_RECTANGLE;
+
         private float xA;
         private float yA;
         private float xB;
         private float yB;
+        private boolean isDrawing = false;
+        private int currentColor = Color.BLACK;
+
         public ViewDraw(Context context)
         {
             super(context);
-            this.setOnTouchListener(this);
-            setFocusable(true);
+            setOnTouchListener(this);
+            setOnClickListener(this);
+            paint.setColor(Color.BLACK);
+            paint.setAntiAlias(true);
+            paint.setStrokeWidth(5);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeJoin(Paint.Join.ROUND);
+            paint.setStrokeCap(Paint.Cap.ROUND);
         }
 
-        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-        public void onDraw (Canvas canvas)
-        {
-            switch (this.currentTool)
-            {
-                case TOOL_RECTANGLE :
-                    canvas.drawRect(xA,yA,xB,yB,paint);
-                    break;
-                case TOOL_ROND:
-                    canvas.drawOval(xA,yA,xB,yB,paint);
-                    break;
-            }
+        @Override
+        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+            super.onSizeChanged(w, h, oldw, oldh);
+            mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            mCanvas = new Canvas(mBitmap);
+            mTempBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            mTempCanvas = new Canvas(mTempBitmap);
+        }
+        @Override
+        public void onClick(View view) {
+            Log.d("Tag","On Click");
 
-            for (Dessin dessin : this.metier.getLstDessin())
-            {
-                switch (dessin.getType())
-                {
-                    case TOOL_RECTANGLE:
-                        canvas.drawRect(dessin.getFromX(),dessin.getFromY(),dessin.getToX(),dessin.getToY(),paint);
-                        break;
-                    case TOOL_ROND:
-                        canvas.drawOval(dessin.getFromX(),dessin.getFromY(),dessin.getToX(),dessin.getToY(),paint);
-                        break;
-                }
-            }
-            for (Path path : this.metier.getLstPath()){
-                switch (this.currentTool)
-                {
-                    case TOOL_TRACE:
-                        paint.setColor(this.currentColor);
-                        break;
-                    case TOOL_ERASER:
-                        paint.setColor(Color.WHITE);
-                        break;
-                }
-                paint.setDither(true);
-                paint.setStyle(Paint.Style.STROKE);
-                paint.setStrokeJoin(Paint.Join.ROUND);
-                paint.setStrokeCap(Paint.Cap.ROUND);
-                paint.setStrokeWidth(3);
-                canvas.drawPath(path,paint);
-            }
         }
 
-
-
-        public boolean onTouch(View view, MotionEvent motionEvent)
-        {
-            //Log.d ("Tag",this.lstPath+"");
-           //Log.d("TAG", "On Touch:"+ "X;"+motionEvent.getX()+" Y:"+motionEvent.getY());
-            float x = motionEvent.getX();
-            float y = motionEvent.getY();
-
-            switch (motionEvent.getAction())
-            {
+        @Override
+        public boolean onTouch(View view,MotionEvent event) {
+            float touchX = event.getX();
+            float touchY = event.getY();
+            //respond to down, move and up events
+            switch (event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
-                    this.xA =  motionEvent.getX();
-                    this.yA =  motionEvent.getY();
-                    if (this.currentTool == TOOL_TRACE || this.currentTool==TOOL_ERASER){
-                        path.moveTo(x,y);
-                        invalidate();
+                    isDrawing = true;
+                    xA = touchX;
+                    yA = touchY;
+                    if (currentTool == TOOL_TRACE) {
+                        path.moveTo(touchX, touchY);
                     }
-                    return true;
+                    invalidate();
+
+                    break;
                 case MotionEvent.ACTION_MOVE:
-                    //Log.d("TAG", "On Touch Move:"+ "X;"+motionEvent.getX()+" Y:"+motionEvent.getY());
-                    this.xB =  motionEvent.getX();
-                    this.yB =  motionEvent.getY();
-                    if (this.currentTool == TOOL_TRACE){
-                        path.lineTo(x,y);
-                        this.metier.addPath(path);
-                    }
-                    invalidate();
-                    return true;
-                case MotionEvent.ACTION_UP:
-                    if (this.currentTool != TOOL_TRACE || this.currentTool!=TOOL_ERASER)
+                    isDrawing = true;
+                    xB = touchX;
+                    yB = touchY;
+                    switch (currentTool)
                     {
-                        this.metier.addDessin(currentTool, this.xA, this.yA, this.xB, this.yB, this.currentColor);
-                        xA = yA = xB = yB = 0;
+                        case TOOL_RECTANGLE:
+                            Paint paintTemp = new Paint();
+                            paintTemp.setColor(Color.parseColor("#330000FF"));
+                            mTempCanvas.drawRect(xA,yA,xB,yB,paintTemp);
+                            break;
+                        case TOOL_TRACE:
+                            path.lineTo(touchX, touchY);
+                            mCanvas.drawPath(path, paint);
+                            break;
+                        case TOOL_ROUND:
+                            Paint paintTemp2 = new Paint();
+                            paintTemp.setColor(Color.parseColor("#330000FF"));
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                                mTempCanvas.drawOval(xA,yA,xB,yB,paintTemp2);
+                            }
+                            break;
+
                     }
+
                     invalidate();
-                    return true;
+
+                    break;
+                case MotionEvent.ACTION_UP:
+                    isDrawing = false;
+
+                    switch (currentTool)
+                    {
+                        case TOOL_RECTANGLE:
+                            mTempBitmap.eraseColor(Color.TRANSPARENT);
+                            paint.setColor(this.currentColor);
+                            mCanvas.drawRect(xA,yA,xB,yB,paint);
+                            break;
+                        case TOOL_TRACE:
+                            paint.setColor(this.currentColor);
+                            path.lineTo(touchX, touchY);
+                            mCanvas.drawPath(path, paint);
+                            path.reset();
+                            break;
+                        case TOOL_ROUND:
+                            paint.setColor(this.currentColor);
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                                mCanvas.drawOval(xA,yA,xB,yB,paint);
+                            }
+                            break;
+
+                    }
+                    xA=xB= touchX;
+                    yA=yB= touchY;
+                    invalidate();
+
+                    break;
                 default:
                     return true;
             }
-
+            //redraw
+            invalidate();
+            return true;
         }
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawBitmap(mBitmap, 0 , 0, paint);
+            if (isDrawing)
+            {
+                switch (currentTool)
+                {
+                    case TOOL_RECTANGLE:
+                        canvas.drawBitmap(mTempBitmap, 0, 0, null);
+                        break;
+                }
 
-        public void onClick(View view) {}
-
-
+            }
+        }
+        public void setColor(int color){
+            this.currentColor = color;
+            paint.setColor(color);
+        }
     }
 
 
-    public void rouge(View view) {paint.setColor(Color.RED);}
+    public void rouge(View view) {whatIdraw.setColor(Color.RED);}
 
-    public void vert(View view) {paint.setColor(Color.GREEN);}
+    public void vert(View view) {whatIdraw.setColor(Color.GREEN);}
 
-    public void bleu(View view) {paint.setColor(Color.BLUE);}
+    public void bleu(View view) {whatIdraw.setColor(Color.BLUE);}
 
-    public void jaune(View view) {paint.setColor(Color.YELLOW);}
+    public void jaune(View view) {whatIdraw.setColor(Color.YELLOW);}
 
-    public void noir(View view) {paint.setColor(Color.BLACK);}
+    public void noir(View view) {whatIdraw.setColor(Color.BLACK);}
 
 
 
